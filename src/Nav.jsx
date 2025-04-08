aa007a342501c7a8406a9ab45ff121d0a822a5bd
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-04-08 17:36:09 +0100

    updt

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,13 +3,17 @@ import { NavLink } from "react-router-dom"
 function NavBar() {
     return (
         <nav className="nav">
-            <h1 className="logo">Rotate</h1>
+            <NavLink className="logo" to="/">Rotate</NavLink>
             <ul className="desktop-nav">
                 <NavLink to="/"><li>home</li></NavLink>
                 <NavLink to="/about"><li>about</li></NavLink>
                 <NavLink to="/courses"><li>courses</li></NavLink>
             </ul>
-            <p>(0)</p>
+
+            <p className="cart">(0)</p>
+            <ul className="mobile-nav">
+            <ion-icon name="menu-outline"></ion-icon>
+            </ul>
         </nav>
     )
 }
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,6 +2,26 @@ function HomeScreen() {
     return (
         <div>
             <h1>I am home, come on now</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam voluptas ut totam saepe quis sed dolore a dolorum eaque! Aperiam exercitationem voluptate laborum iure explicabo veritatis aliquid error vel nobis.
+            Tempora in cumque, expedita, rem quas a eius sint non ut consectetur culpa quisquam! Sunt debitis optio ex tempore animi obcaecati deserunt, aspernatur quas atque fugit consequatur deleniti repudiandae molestias!
+            Placeat eos minima quis excepturi doloribus facilis ex ab itaque a quidem! Autem architecto reprehenderit voluptates rem repudiandae, sint, explicabo consequatur quis itaque nemo velit iusto ipsa animi! Eius, rerum.
+            Repellendus odit nobis, mollitia ipsa reiciendis porro facere, rerum, in veniam corporis nostrum eligendi culpa explicabo. Fuga possimus quos perferendis adipisci voluptatum perspiciatis cumque ea quae, libero cupiditate vitae tempore!
+            Porro rem amet dolorem, sed minus voluptatum deleniti iusto fugit non repudiandae sint quibusdam odit dolores velit, exercitationem placeat eligendi! Eum, saepe. Totam ipsa recusandae error assumenda, corrupti nisi nostrum.
+            Et praesentium voluptatem omnis esse quo dolorem minima cumque blanditiis veniam a unde officiis aut ad dolore consequuntur doloremque nobis, accusantium iusto distinctio ipsam dignissimos corporis earum. Voluptatibus, facilis nihil!
+            Nesciunt facere quos beatae, expedita aspernatur iure dolor dignissimos itaque odio laborum commodi odit neque nam voluptas animi ex sapiente aut magnam placeat tenetur? Rem ducimus quasi obcaecati nisi officiis.
+            Minus nemo facere ullam exercitationem, ab delectus magnam commodi quidem fugit necessitatibus, rerum sed sapiente animi maiores saepe quam temporibus. Consequuntur dolor repellendus, itaque eaque aliquid vitae exercitationem totam laboriosam!
+            Omnis nesciunt animi totam culpa, voluptatum voluptate illo a quibusdam natus laudantium expedita unde aut consequatur enim deserunt, aliquid asperiores porro veniam. Culpa pariatur quo quae ipsa nihil deleniti officia.
+            Omnis at reprehenderit tempore non similique debitis unde totam vero, odio illum tempora culpa repudiandae accusamus. Obcaecati expedita, sunt quam eligendi autem assumenda unde, dolor dolore velit sequi nemo ex!
+            Alias tempora accusamus eveniet? Neque cupiditate expedita ducimus eligendi laborum aut voluptates, molestiae doloremque deserunt, facere similique blanditiis sit dignissimos reprehenderit! Voluptate deleniti tenetur cupiditate voluptatibus! Dolore officiis quae id.
+            Quaerat molestias sit similique fuga maxime unde totam deserunt officia fugiat recusandae? Vel quisquam eum sed non mollitia veniam, debitis illum at, nihil quae soluta dicta rem. Laborum, accusamus tempore!
+            Quaerat iure, ipsam laboriosam quibusdam earum necessitatibus ex inventore, possimus, obcaecati ullam nesciunt iste aut reiciendis quidem expedita laborum impedit placeat laudantium! Nobis repellat harum voluptates adipisci deleniti. Recusandae, qui.
+            Dolores repellat soluta natus possimus nostrum quae similique doloribus saepe vitae deleniti ratione, a hic delectus provident iure cupiditate dignissimos! Quam voluptas sit, dolores illo at rerum a libero quia!
+            Dolores placeat perspiciatis voluptates, dignissimos incidunt culpa, nulla in repellat adipisci, vel aperiam sed consequatur nam. Molestiae, odit tempore architecto voluptatibus illo eligendi, reiciendis necessitatibus explicabo, asperiores doloribus nisi. Iste.
+            Accusamus sequi tempora mollitia magnam exercitationem adipisci cumque quas. Quod, harum tempore consequatur, quidem a nesciunt vitae neque atque voluptatum, ipsa accusamus nulla magnam similique repudiandae sed dolor esse reiciendis.
+            Quas dicta unde ea sed molestiae numquam odit nam. Aspernatur similique, voluptates modi officiis porro ratione nobis suscipit cupiditate facilis! Accusamus eligendi error iste quasi natus. Repudiandae eum ab nostrum!
+            Ipsam et odio enim optio delectus quasi! Quam maxime, blanditiis voluptas harum nisi debitis a alias aut odit, expedita perferendis quod nihil fugiat, id at. Culpa est sunt debitis ipsum.
+            Numquam atque, quae cupiditate, dolore sit rem labore molestiae adipisci accusantium corrupti iste laboriosam recusandae sapiente est ipsam, aspernatur hic nam deserunt nesciunt expedita assumenda architecto. Repellendus sapiente sit animi.
+            Eius maiores, aliquid nihil quibusdam perspiciatis corrupti reprehenderit esse explicabo. Architecto, illo veniam corporis necessitatibus facere ipsum quisquam suscipit molestias! Odit accusamus at molestiae magni obcaecati labore deleniti commodi quos!</p>
         </div>
     )
 }
