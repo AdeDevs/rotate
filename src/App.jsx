6919016e9336711e959f753bd6feae825890c7c8
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-04-08 16:14:06 +0100

    updt

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <p>This is a simple React application.</p>
+      <p>Welcome to my app!</p>
+      <p>Enjoy your stay!</p>
+      <p>Have a great day!</p>
+      <p>Thank you for visiting!</p>
+      <p>Feel free to explore!</p>
+    </div>
+  )
+}
+
+export default App
