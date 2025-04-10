dfa707a5cfda19634833a63428479fdb518f7183
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-04-10 01:34:57 +0100

    updt

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -7,6 +7,19 @@ function HomeScreen() {
                     <h1>Rotate</h1>
                 </div>
             </header>
+            <main className="home-main">
+                <section className="we-are">
+                    <div>
+                        <h1>we're rotate studio. let's dance!</h1>
+                        <p>
+                            Our mission is to build community through dance. Rotate Studio offers beautifully-designed spaces to move, build, and socialize. We embrace diversity and body-positivity: all athletes are always welcome here.
+                        </p>
+                        <button>view schedule</button>
+                    </div>
+                    <div className="right-box"></div>
+                    <div></div>
+                </section>
+            </main>
         </div>
     )
 }
