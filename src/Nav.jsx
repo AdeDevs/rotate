7d4e65002b6e80260e9f04dced0cda4f8c660d92
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-04-11 20:07:11 +0100

    updt

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react"
 import { NavLink } from "react-router-dom"
 
 function NavBar() {
+    const [menuActive, setMenuActive] = useState(false)
+    const toggleMenu = () => {
+        setMenuActive(!menuActive)
+    }
+
     return (
-        <nav className="nav">
+        <nav className={`nav ${menuActive ? "active" : ""}`}>
             <NavLink className="logo" to="/">Rotate studio</NavLink>
             <ul className="desktop-nav">
                 <NavLink to="/class"><li>class</li></NavLink>
@@ -13,10 +19,24 @@ function NavBar() {
             </ul>
             <ul className="nav-extras">
                 <li className="cart">(0)</li>
-                <li><span className="open-menu"><ion-icon name="menu-outline"></ion-icon></span></li>
+                <li onClick={toggleMenu}>
+                    <span className="open-menu"><ion-icon name="menu-outline"></ion-icon></span>
+                </li>
             </ul>
-            <ul className="mobile-nav">
-            <ion-icon name="close-outline"></ion-icon>
+            <ul className={`mobile-nav ${menuActive ? "active" : ""}`}>
+                <div className="mobile-head">
+                <NavLink className="logo" to="/" onClick={toggleMenu}>Rotate studio</NavLink>
+                <ul className="nav-extras">
+                <li className="cart" onClick={toggleMenu}>(0)</li>
+                <li onClick={toggleMenu}>
+                    <span className="open-menu"><ion-icon name="menu-outline"></ion-icon></span>
+                </li>
+            </ul>
+                </div>
+                <li onClick={toggleMenu}><NavLink to="/class">classes</NavLink></li>
+                <li onClick={toggleMenu}><NavLink to="/schedule">schedule</NavLink></li>
+                <li onClick={toggleMenu}><NavLink to="/about">about</NavLink></li>
+                <li onClick={toggleMenu}><NavLink to="/merch">merch</NavLink></li>
             </ul>
         </nav>
     )
