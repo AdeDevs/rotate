d26de372805d9224e062dd1037f8b9cce441c9bc
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-04-13 15:02:53 +0100

    updt

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './styles/index.css'
 import './styles/reset.css'
 import './styles/home.css'
 import './styles/classes.css'
+import './styles/about.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,7 +1,29 @@
+import Footer from "./Footer";
+
 function AboutPage() {
     return (
         <div className="about-page">
-            <h1>We are going to be telling you a bit about us</h1>
+            <header className="hero">
+                <div className="hero-bg"></div>
+                <div>
+                    <h1>about us</h1>
+                </div>
+            </header>
+            <main className="about-main">
+                <div className="about-fitness">
+                <div className="founder-bg"></div>
+                    <div className="fitness-info">
+                        <h1>fitness for all</h1>
+                        <p>
+                            Rotate Studio is a dance and fitness studio for BODIES of all abilities, genders, races, shapes, and sizes located in the heart of HOUSTON, TEXAS.
+                            <br /><br />
+                            Our mission is to build community, acceptance and culture through dance. ROTATE STUDIO offers beautifully-designed spaces to move, build, and socialize. We embrace diversity and body-positivity: all athletes are always welcome here.
+                        </p>
+                    </div>
+                </div>
+                <h6>INSPIRING STRENGTH and BUILDING community through judgment-free dance</h6>
+            </main>
+            <Footer />
         </div>
     )
 }
